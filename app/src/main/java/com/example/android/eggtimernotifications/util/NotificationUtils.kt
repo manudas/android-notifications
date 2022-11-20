727f71cedb18b7f89b7dcb2dabafabcaf59f942f
Manuel <cibermanu@hotmail.com>
2022-11-20 17:14:54 +0100

    Added Intent / Pending intent to open up Main Activity on user clicking in the notification

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -42,8 +42,17 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // Create the content intent for the notification, which launches
     // this activity
     // TODO: Step 1.11 create intent
-
+    val contentIntent = Intent(
+        applicationContext,
+        MainActivity::class.java
+    )
     // TODO: Step 1.12 create PendingIntent
+    val pendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     // TODO: Step 2.0 add style
 
@@ -62,7 +71,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
     // TODO: Step 1.13 set content intent
-
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
         // TODO: Step 2.1 add style to builder
 
         // TODO: Step 2.3 add snooze action
